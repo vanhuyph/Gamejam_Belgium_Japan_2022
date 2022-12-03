11b15de0e43f00d43014100a22b34ed2ee6dee58
Takiidesuu <rickylieandro@yahoo.com>
2022-12-03 15:51:49 +0900

    Test level

--- Gamejam/Assets/Scripts/CameraScript.cs
+++ Gamejam/Assets/Scripts/CameraScript.cs
@@ -6,6 +6,9 @@ public class CameraScript : MonoBehaviour
 {
     GameObject playerObj;
     
+    [SerializeField] float minPos = -5.0f;
+    [SerializeField] float maxPos = 100.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +18,6 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        var minPos = -2.0f;
-        var maxPos = 20.0f;
-        
         transform.position = playerObj.transform.position + new Vector3(0.0f, 2.0f, -10.0f);
         
         var posX = Mathf.Clamp(transform.position.x, minPos, maxPos);
