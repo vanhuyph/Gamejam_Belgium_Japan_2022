61657d241eb8e19d4e0ca9e5ba5137f8cce9b1e0
vanhuyph <vanhuypham26@gmail.com>
2022-12-04 12:50:49 +0100

    fixed lobby scene

--- Gamejam/Assets/Scripts/waterdoor.cs
+++ Gamejam/Assets/Scripts/waterdoor.cs
@@ -1,14 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
- 
-public class waterdoor : MonoBehaviour
+
+public class WaterDoor : MonoBehaviour
 {
     public int sceneBuildIndex;
- 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag == "Player") {
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
             SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
         }
     }
